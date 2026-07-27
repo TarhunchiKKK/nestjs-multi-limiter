fb65aef2679a43c7b607937f34d7e1e2009ebba9
TarhunchiKKK <kostabarilo12@gmail.com>
2026-07-27 12:27:15 +0300

    docs: docs for sliding window count strategy types

--- lib/src/executors/variants/sliding-window-counter/types.ts
+++ lib/src/executors/variants/sliding-window-counter/types.ts
@@ -1,10 +1,32 @@
+/**
+ * @publicApi
+ */
 export type SlidingWindowCounterOptions = {
+    /**
+     * Maximum requests count per window.
+     */
     limit: number;
+
+    /**
+     * Window length in milliseconds.
+     * Example: 60000 for 1 minute.
+     */
     windowMs: number;
 };
 
 export type SlidingWindowCounterState = {
+    /**
+     * Current window start timestamp.
+     */
     currentWindowStart: number;
+
+    /**
+     * Current window equests counter.
+     */
     currentCount: number;
+
+    /**
+     * Previous window requests counter.
+     */
     previousCont: number;
 };
