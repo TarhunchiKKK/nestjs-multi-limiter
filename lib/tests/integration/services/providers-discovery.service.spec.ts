43f4ae75aa00609a2ba4f7e3da0d7590057e3f4c
TarhunchiKKK <kostabarilo12@gmail.com>
2026-07-27 21:37:36 +0300

    test(integration): add built-in providers disovery test cases for `ProvidersDiscoveryService` provider

--- lib/tests/integration/services/providers-discovery.service.spec.ts
+++ lib/tests/integration/services/providers-discovery.service.spec.ts
@@ -1,9 +1,10 @@
-import { afterEach, beforeEach, describe, expect, it } from "bun:test";
-import { DiscoveryService, ModulesContainer, Reflector } from "@nestjs/core";
+import { beforeAll, beforeEach, describe, expect, it } from "bun:test";
+import { ModulesContainer, Reflector } from "@nestjs/core";
 import { Test } from "@nestjs/testing";
 import { RateLimiterModule, type RateLimiterModuleOptions, type StorageTypes, type Strategies } from "../../../src";
 import { RATE_LIMITER_MODULE_DEFAULT_OPTIONS } from "../../../src/config/defaults/default-options.constants";
-import { MODULE_OPTIONS_TOKEN } from "../../../src/di";
+import { BuiltinErrorFactory } from "../../../src/custom/error-factories";
+import { BuiltinKeyExtractor } from "../../../src/custom/key-extractors";
 import { ProvidersDiscoveryService } from "../../../src/services/providers-discovery.service";
 import { createRedisClient } from "../../shared";
 
@@ -13,19 +14,21 @@ describe("ProvidersDiscoveryService", () => {
     describe.each<[StorageTypes, RateLimiterModuleOptions]>([
         ["in-memory", RATE_LIMITER_MODULE_DEFAULT_OPTIONS],
         ["redis", { ...RATE_LIMITER_MODULE_DEFAULT_OPTIONS, storage: "redis", instance: createRedisClient() }]
-    ])("common cases", (storageType, options) => {
+    ])("executors discovery", (storageType, options) => {
         let service: ProvidersDiscoveryService;
 
         beforeEach(async () => {
             const module = await Test.createTestingModule({
                 imports: [RateLimiterModule.forRoot(options)],
+
+                // DELETE: are this providers necessary?
                 providers: [ModulesContainer, Reflector]
             }).compile();
 
             service = module.get(ProvidersDiscoveryService);
         });
 
-        it(`should find all executors (${storageType} storage)`, async () => {
+        it(`should find all executors (${storageType} storage)`, () => {
             for (const strategy of strategies) {
                 const executor = service.getExecutor(strategy);
 
@@ -35,7 +38,34 @@ describe("ProvidersDiscoveryService", () => {
         });
     });
 
-    describe("with builtin providers", () => {});
+    describe("built-in providers discovery", () => {
+        let service: ProvidersDiscoveryService;
+
+        beforeAll(async () => {
+            const module = await Test.createTestingModule({
+                imports: [RateLimiterModule.forRoot(RATE_LIMITER_MODULE_DEFAULT_OPTIONS)],
+
+                // DELETE: are this providers necessary?
+                providers: [ModulesContainer, Reflector]
+            }).compile();
+
+            service = module.get(ProvidersDiscoveryService);
+        });
+
+        it("should find built-in key extractor", () => {
+            const keyExtractor = service.getKeyExtractor(BuiltinKeyExtractor);
+
+            expect(keyExtractor).toBeDefined();
+            expect(keyExtractor.extract).toBeFunction();
+        });
+
+        it("should find built-in error factory", () => {
+            const errorFactory = service.getErrorFactory(BuiltinErrorFactory);
+
+            expect(errorFactory).toBeDefined();
+            expect(errorFactory.getError).toBeFunction();
+        });
+    });
 
     describe("with custom providers", () => {});
 });
