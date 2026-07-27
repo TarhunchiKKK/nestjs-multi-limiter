c356bc368c5592eb9f171375056e8922963c02d1
TarhunchiKKK <kostabarilo12@gmail.com>
2026-07-27 12:19:57 +0300

    docs: docs for fixed window strategy types

--- lib/src/executors/variants/fixed-window/types.ts
+++ lib/src/executors/variants/fixed-window/types.ts
@@ -1,9 +1,26 @@
+/**
+ * @public
+ */
 export type FixedWindowOptions = {
+    /**
+     * Maximum requests count
+     */
     limit: number;
+
+    /**
+     * Requests limit time-to-live
+     */
     ttl: number;
 };
 
 export type FixedWindowState = {
+    /**
+     * Current allowed requests count
+     */
     count: number;
+
+    /**
+     * Last attemptions reset time
+     */
     resetTime: number;
 };
