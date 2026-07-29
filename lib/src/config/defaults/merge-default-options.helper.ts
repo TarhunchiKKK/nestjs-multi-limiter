1b9d77eb67b9741ade824bd9a71cfac8d80c35c5
TarhunchiKKK <kostabarilo12@gmail.com>
2026-07-29 16:12:23 +0300

    chore: setup `husky`

--- lib/src/config/defaults/merge-default-options.helper.ts
+++ lib/src/config/defaults/merge-default-options.helper.ts
@@ -35,9 +35,8 @@ export function mergeDefaultOptions(options: RateLimiterModuleOptions) {
         },
 
         defaultProviders: {
-            keyExtractor: options?.defaultProviders?.keyExtractor ?? RATE_LIMITER_MODULE_DEFAULT_OPTIONS?.defaultProviders?.keyExtractor,
-            errorFactory: options?.defaultProviders?.errorFactory ?? RATE_LIMITER_MODULE_DEFAULT_OPTIONS?.defaultProviders?.errorFactory,
-            optionsFactory: options?.defaultProviders?.optionsFactory ?? RATE_LIMITER_MODULE_DEFAULT_OPTIONS?.defaultProviders?.optionsFactory
+            ...RATE_LIMITER_MODULE_DEFAULT_OPTIONS.defaultProviders,
+            ...options.defaultProviders
         }
     } satisfies RateLimiterModuleFullOptions;
 }
