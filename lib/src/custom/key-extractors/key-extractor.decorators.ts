9b5f7224b540bf3ddae9480f02dcee2600341b43
TarhunchiKKK <kostabarilo12@gmail.com>
2026-07-29 15:12:44 +0300

    style: formatting

--- lib/src/custom/key-extractors/key-extractor.decorators.ts
+++ lib/src/custom/key-extractors/key-extractor.decorators.ts
@@ -5,9 +5,9 @@ export const KEY_EXTRACTOR_METADATA = "__rate_limiter_key_extractor__";
 
 /**
  * Decorator that marks class as custom key extractor.
- * 
- * @param options Options that are similar to `@Injectable()` decorator options. 
- * 
+ *
+ * @param options Options that are similar to `@Injectable()` decorator options.
+ *
  * @publicApi
  */
 export function KeyExtractor(options?: InjectableOptions): ClassDecorator {
