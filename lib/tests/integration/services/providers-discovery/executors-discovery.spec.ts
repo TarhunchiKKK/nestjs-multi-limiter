bf0728e7ebeb5310fab11dd17a4db3cc8907b0f3
TarhunchiKKK <kostabarilo12@gmail.com>
2026-07-31 09:42:23 +0300

    test(integration): fix test cases for `ProvidersDiscoveryService` provider

--- lib/tests/integration/services/providers-discovery/executors-discovery.spec.ts
+++ lib/tests/integration/services/providers-discovery/executors-discovery.spec.ts
@@ -1,5 +1,5 @@
-import { beforeEach, describe, expect, it } from "bun:test";
-import { Test } from "@nestjs/testing";
+import { afterEach, beforeEach, describe, expect, it } from "bun:test";
+import { Test, type TestingModule } from "@nestjs/testing";
 import { RateLimiterModule, type RateLimiterModuleOptions, type StorageTypes, type Strategies } from "../../../../src";
 import { RATE_LIMITER_MODULE_DEFAULT_OPTIONS } from "../../../../src/config/defaults/default-options.constants";
 import { ProvidersDiscoveryService } from "../../../../src/services/providers-discovery.service";
@@ -12,13 +12,20 @@ describe.each<[StorageTypes, RateLimiterModuleOptions]>([
     ["redis", { ...RATE_LIMITER_MODULE_DEFAULT_OPTIONS, storage: { type: "redis", adapter: createRedisClient() } }]
 ])("ProvidersDiscoveryService - executors discovery (%1 storage)", (storageType, options) => {
     let service: ProvidersDiscoveryService;
+    let module: TestingModule;
 
     beforeEach(async () => {
-        const module = await Test.createTestingModule({
+        module = await Test.createTestingModule({
             imports: [RateLimiterModule.forRoot(options)]
         }).compile();
 
         service = module.get(ProvidersDiscoveryService);
+
+        await module.init();
+    });
+
+    afterEach(async () => {
+        await module.close();
     });
 
     it(`should find all executors (${storageType} storage)`, () => {
