a7378dbace66b866ec34a87f7ae8e7a2248d6e2f
TarhunchiKKK <kostabarilo12@gmail.com>
2026-08-05 07:15:36 +0300

    ci: fix knip error

--- lib/tests/shared/constants/date.constants.ts
+++ lib/tests/shared/constants/date.constants.ts
@@ -3,7 +3,3 @@ export const MS_IN_SECOND = 1000;
 export const MS_IN_MINUTE = 1 * 60 * 1000;
 
 export const MS_IN_DAY = 1 * 24 * 60 * 60 * 1000;
-
-export const TOMORROW = Date.now() + MS_IN_DAY;
-
-export const YESTERDAY = Date.now() - MS_IN_DAY;
