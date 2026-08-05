124ad043f31909fc0be434dfeb0786cb34c2e950
TarhunchiKKK <kostabarilo12@gmail.com>
2026-08-05 12:10:19 +0300

    test(e2e): remove controllers

--- e2e/src/controllers/skipping.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller, Get } from "@nestjs/common";
-
-@Controller("skipping")
-export class SkippingController {
-    @Get("skip")
-    public skip() {}
-}
--- e2e/src/custom-providers.module.ts
+++ e2e/src/custom-providers.module.ts
@@ -1,13 +1,9 @@
 import { Module } from "@nestjs/common";
-import { DefaultProvidersController } from "./controllers/default-providers.controller";
-import { OverrideProvidersController } from "./controllers/override-providers.controller";
-import { SkippingController } from "./controllers/skipping.controller";
 import { MoviesModule } from "./movies/movies.module";
 import { RedisModule } from "./redis/redis.module";
 import { UsersModule } from "./users/users.module";
 
 @Module({
-    imports: [UsersModule, MoviesModule, RedisModule],
-    controllers: [DefaultProvidersController, OverrideProvidersController, SkippingController]
+    imports: [UsersModule, MoviesModule, RedisModule]
 })
 export class CustomProvidersModule {}
