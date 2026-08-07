9cf5d1c02c8b3c4a6a37b35b591e95d1a9f39809
TarhunchiKKK <kostabarilo12@gmail.com>
2026-08-07 19:16:06 +0300

    fix(di): update types for default providers tokens

--- lib/src/config/options/decorator.options.ts
+++ lib/src/config/options/decorator.options.ts
@@ -1,9 +1,9 @@
-import type { Type } from "@nestjs/common";
+import type { InjectionToken } from "@nestjs/common";
 import type { IErrorFactory } from "../../custom/error-factories";
 import type { IKeyExtractor } from "../../custom/key-extractors";
 import type { IOptionsFactory } from "../../custom/options-factories";
 import type { StrategyOptionsUnion } from "../../executors";
-import type { DeepPartial, PartialUnionMembers, TokenType } from "../../shared/lib";
+import type { DeepPartial, PartialUnionMembers } from "../../shared/lib";
 import type { Scope } from "../../shared/model";
 import type { StrategyOptions } from "./common.options";
 
@@ -21,17 +21,17 @@ export type RateLimitOptions = {
     /**
      * Overrides default key extractor.
      */
-    keyExtractor?: Type<IKeyExtractor> | TokenType;
+    keyExtractor?: InjectionToken<IKeyExtractor>;
 
     /**
      * Overrides default error factory.
      */
-    errorFactory?: Type<IErrorFactory> | TokenType;
+    errorFactory?: InjectionToken<IErrorFactory>;
 
     /**
      * Overrides default options factory
      */
-    factory?: Type<IOptionsFactory> | TokenType;
+    factory?: InjectionToken<IOptionsFactory>;
 } & PartialUnionMembers<StrategyOptionsUnion>;
 
 export type RateLimitNormalizedOptions = Pick<RateLimitOptions, "scope" | "keyExtractor" | "errorFactory" | "factory"> & DeepPartial<StrategyOptions>;
