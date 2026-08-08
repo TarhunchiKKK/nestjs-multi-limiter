288b7ac031f3d3909f76d0bedf57bcb8df5c3fa8
TarhunchiKKK <kostabarilo12@gmail.com>
2026-08-08 19:18:43 +0300

    fix(lib): replace `reflector.get` with `reflector.getAllAndOverride` in `rateLimitGuard` class

--- lib/src/middleware/rate-limit.guard.ts
+++ lib/src/middleware/rate-limit.guard.ts
@@ -56,7 +56,7 @@ export class RateLimitGuard implements CanActivate {
     }
 
     private checkSkip(context: ExecutionContext) {
-        const shouldSkip = this.reflector.get(SkipRateLimit, context.getHandler());
+        const shouldSkip = this.reflector.getAllAndOverride(SkipRateLimit, [context.getHandler(), context.getClass()]);
 
         if (shouldSkip) {
             return true;
@@ -66,7 +66,7 @@ export class RateLimitGuard implements CanActivate {
     }
 
     private async getOptions(context: ExecutionContext): Promise<RunOptions> {
-        const options = this.reflector.get(RateLimit, context.getHandler());
+        const options = this.reflector.getAllAndOverride(RateLimit, [context.getHandler(), context.getClass()]);
 
         if (!options) {
             return {
