c8bbf092311ad01b74ce0f4364eb076e7bb6deac
TarhunchiKKK <kostabarilo12@gmail.com>
2026-08-26 06:42:29 +0300

    feat(lib): create `RateLimitDynamicOptions` type

---
+++ TODO.md
@@ -0,0 +1,4 @@
+1. Options merging
+2. Docs for bypassing
+3. Docs for `@RateLimit` decorator new interface (`options` field)
+4. E2e-specs for options merging
--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,7 +1,7 @@
 export type { IRateLimiterModuleOptionsFactory, RateLimiterModuleAsyncOptions, RateLimiterModuleOptions } from "./config";
 export { ErrorFactory, type ErrorFactoryOptions, type IErrorFactory } from "./custom/error-factories";
 export { type IKeyExtractor, KeyExtractor } from "./custom/key-extractors";
-export { type IOptionsFactory, OptionsFactory } from "./custom/options-factories";
+export { type DynamicRateLimitOptions, type IOptionsFactory, OptionsFactory } from "./custom/options-factories";
 export { RateLimit, type RateLimitOptions, RejectRateLimit, SkipRateLimit } from "./decorators";
 export { RateLimitGuard } from "./rate-limit.guard";
 export { RateLimiterModule } from "./rate-limiter.module";
