d0f68520a022da5a5fa9e2e35a86ea09e4bbaa72
Konstantin Barilo <kostabarilo12@gmail.com>
2026-09-02 07:24:07 +0000

    feat(lib): create `RedisAdapterError` class

--- lib/src/shared/errors/index.ts
+++ lib/src/shared/errors/index.ts
@@ -1,5 +1,6 @@
 export { IncorrectLuaScriptResultError } from "./incorrect-lua-script-result.error";
 export { InvalidAsyncConfigurationError } from "./invalid-async-configuration.error";
 export { RateLimiterModuleConfigurationError } from "./rate-limiter-module-configuration.error";
+export { RedisAdapterError } from "./redis-adapter.error";
 export { UnknownRedisFailingStrategyError } from "./unknown-redis-failing-strategy.error";
 export { UnknownRateLimitStorageError } from "./unknown-storage-type.error";
